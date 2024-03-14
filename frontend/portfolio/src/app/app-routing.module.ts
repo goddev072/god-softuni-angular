e1937b9047469928c28a7312c6eeae73d70b93c8
goddev072 <god.dev0912@outlook.com>
2024-03-14 07:16:07 +0200

    frontend files

--- frontend/portfolio/src/app/app-routing.module.ts
+++ frontend/portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AboutComponent } from './about/about.component';
+import { NoFoundComponent } from './no-found/no-found.component';
+import { ProjectComponent } from './project/project.component';
+import { ContactComponent } from './contact/contact.component';
+import { LogoutComponent } from './logout/logout.component';
+import { mainGuard } from './auth/main.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'about-me', component: AboutComponent },
+  { path: 'contact-me', component: ContactComponent },
+  { path: 'projects', canActivate: [mainGuard], component: ProjectComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: '**', component: NoFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
