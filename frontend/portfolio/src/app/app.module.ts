f0d2ebee538fa13a27b5acfef8e15af68189e853
goddev072 <god.dev0912@outlook.com>
2024-03-14 07:16:03 +0200

    frontend files

--- frontend/portfolio/src/app/app.module.ts
+++ frontend/portfolio/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from "./navigation/navigation.component";
+import { FooterComponent } from "./footer/footer.component";
+import { environment } from 'src/environments/environment.development';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NavigationComponent,
+    FooterComponent,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule, // for firestore
+
   ],
   providers: [],
   bootstrap: [AppComponent]
