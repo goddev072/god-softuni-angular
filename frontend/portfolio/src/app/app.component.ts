df048fcbd8cbb7e1dfae2d5228cffe2410fb07c6
goddev072 <god.dev0912@outlook.com>
2024-03-14 07:16:00 +0200

    frontend files

--- frontend/portfolio/src/app/app.component.ts
+++ frontend/portfolio/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portfolio';
+  portfolio = 'Portfolio';
+  name = 'Dimitar Ivanov Dimitrov'
 }
