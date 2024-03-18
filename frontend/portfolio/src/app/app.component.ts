657095ba2b55114d41e8068d48e063496b9a99a2
goddev072 <god.dev0912@outlook.com>
2024-03-18 07:57:00 +0200

    frontend files

--- frontend/portfolio/src/app/app.component.ts
+++ frontend/portfolio/src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { loadingSub } from "./utils";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  portfolio = 'Portfolio';
-  name = 'Dimitar Ivanov Dimitrov'
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    loadingSub.subscribe(loading => this.loading = loading);
+  }
+
+  protected portfolio = 'Portfolio';
+  protected name = 'Dimitar Ivanov Dimitrov'
+  protected loading: boolean = false;
 }
