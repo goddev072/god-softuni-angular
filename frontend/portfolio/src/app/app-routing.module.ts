1707ba732bb81017fda820952613fc230e5a4e82
goddev072 <god.dev0912@outlook.com>
2024-03-18 07:57:06 +0200

    frontend files

--- frontend/portfolio/src/app/app-routing.module.ts
+++ frontend/portfolio/src/app/app-routing.module.ts
@@ -5,20 +5,22 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AboutComponent } from './about/about.component';
 import { NoFoundComponent } from './no-found/no-found.component';
-import { ProjectComponent } from './project/project.component';
 import { ContactComponent } from './contact/contact.component';
 import { LogoutComponent } from './logout/logout.component';
 import { mainGuard } from './auth/main.guard';
+import { ProjectComponent } from "./project/project.component";
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from "@angular/fire/compat/auth-guard";
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login'])
 
 const routes: Routes = [
-
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent },
   { path: 'about-me', component: AboutComponent },
   { path: 'contact-me', component: ContactComponent },
-  { path: 'projects', canActivate: [mainGuard], component: ProjectComponent },
+  { path: 'projects', canActivate: [mainGuard, AngularFireAuthGuard], component: ProjectComponent, data: { authGuardPipe: redirectUnauthorizedToLogin}},
   { path: 'logout', component: LogoutComponent },
   { path: '**', component: NoFoundComponent },
 ];
