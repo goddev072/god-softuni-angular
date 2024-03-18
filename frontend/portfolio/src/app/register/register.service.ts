0c34793757926a49449c8abb9c4b82c6a8252819
goddev072 <god.dev0912@outlook.com>
2024-03-18 07:59:58 +0200

    frontend files

--- frontend/portfolio/src/app/register/register.service.ts
+++ frontend/portfolio/src/app/register/register.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AbstractService } from '../common/abstract.service';
-import { HttpClient } from "@angular/common/http";
-import { Environment } from "@angular/cli/lib/config/workspace-schema";
-import { environment } from "../../environments/environment.development";
 import { RegisterUser } from "./register.component";
+import { AuthenticationService } from "../auth/authentication.service";
 
 
 @Injectable({
@@ -13,28 +11,11 @@ export class RegisterService extends AbstractService {
 
   private readonly _resourceUrl = "/users/";
 
-  constructor(private client: HttpClient) {
+  constructor(private authSvc: AuthenticationService) {
     super();
   }
 
-
   doRegister(user: RegisterUser) {
-
-    console.log(this.database.list("users").query.get().then(data => data.forEach( a => console.log(a.val()))));
-
-    // let a = ((b: any, c: any) => {
-    //   console.log(b);
-    //   console.log(c);
-    // });
-    //
-    // this.database.list("users").push(user).on('child_added', a);
+    this.authSvc.createUser(user);
   }
 }
-
-
-
-// this._database.object('idGenerator').set(this.generatorId++);
-// this._database.list("users/test34").valueChanges().subscribe((data:any) => console.log(data));
-// console.log(this.generatorId);
-// this.client.get( environment.firebaseConfig.databaseURL + this._resourceUrl +  'test24' + this.suffixDatabaseUrl).subscribe(data => console.log(data));
-// this._database.object('idGenerator').valueChanges().subscribe((data: any) => this.generatorId = data);
