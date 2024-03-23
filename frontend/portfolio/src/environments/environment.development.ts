d54de886ac890d0d696d8c11c52e10258d4e04d5
goddev072 <god.dev0912@outlook.com>
2024-03-23 09:46:51 +0200

    Update environment.development.ts

--- frontend/portfolio/src/environments/environment.development.ts
+++ frontend/portfolio/src/environments/environment.development.ts
@@ -1,12 +1,11 @@
 export const environment = {
   firebaseConfig : {
-    apiKey: "AIzaSyBV73gqpTYDtmqyuXceEFW2z8DEch2QSqg",
-    authDomain: "softuni-angular-project-c8664.firebaseapp.com",
-    databaseURL: "https://softuni-angular-project-c8664-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "softuni-angular-project-c8664",
-    storageBucket: "softuni-angular-project-c8664.appspot.com",
-    messagingSenderId: "193666384659",
-    appId: "1:193666384659:web:b020c2fc63603a0e3490e6"
+    apiKey: "<api-key>",
+    authDomain: "<authDomain>",
+    databaseURL: "<databaseURL>",
+    projectId: "<projectId>",
+    storageBucket: "<storageBucket>",
+    messagingSenderId: "<messagingSenderId>",
+    appId: "<appId>"
   }
 };
-
