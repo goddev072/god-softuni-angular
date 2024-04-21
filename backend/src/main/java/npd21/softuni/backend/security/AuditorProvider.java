d8aec72f5095c595c4b4cf2c8facd199d20e7c81
goddev072 <god.dev0912@outlook.com>
2024-04-21 09:57:44 +0300

    java backend

---
+++ backend/src/main/java/npd21/softuni/backend/security/AuditorProvider.java
@@ -0,0 +1,28 @@
+package npd21.softuni.backend.security;
+
+import npd21.softuni.backend.entities.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class AuditorProvider implements AuditorAware<UUID> {
+
+    @Override
+    public Optional<UUID> getCurrentAuditor() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication == null ||
+                !authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken
+        ) {
+            return Optional.empty();
+        }
+
+        User user = (User) authentication.getPrincipal();
+        return Optional.ofNullable(user.getId());
+    }
+}
