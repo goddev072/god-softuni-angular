e76e823707dc265b072ae3381f020a157e3ec640
goddev072 <god.dev0912@outlook.com>
2024-04-21 09:57:49 +0300

    java backend

---
+++ backend/src/main/java/npd21/softuni/backend/configuration/AllowedUrls.java
@@ -0,0 +1,30 @@
+package npd21.softuni.backend.configuration;
+
+import lombok.Getter;
+
+public class AllowedUrls {
+
+    private static final String[] allowedUrls = {
+        "/api/v1/rest/auth/**",
+        "/api/v1/rest/test",
+        "/test/**",
+        "test",
+        "/v2/api-docs",
+        "/v3/api-docs",
+        "/v3/api-docs/**",
+        "/swagger-resources",
+        "/swagger-resources/**",
+        "/configuration/ui",
+        "/configuration/security",
+        "/swagger-ui/**",
+        "/webjars/**",
+        "/swagger-ui.html"
+    };
+
+    @Getter
+    private static final String logOutUrl = "/api/v1/rest/auth/logout";
+
+    public static String[] getAllowedUrls() {
+        return allowedUrls;
+    }
+}
