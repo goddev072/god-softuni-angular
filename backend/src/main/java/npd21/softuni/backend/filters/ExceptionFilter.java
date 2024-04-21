306d3c1c5b6c33f6529a99d74e6597226cee3fea
goddev072 <god.dev0912@outlook.com>
2024-04-21 09:58:10 +0300

    java backend

---
+++ backend/src/main/java/npd21/softuni/backend/filters/ExceptionFilter.java
@@ -0,0 +1,24 @@
+package npd21.softuni.backend.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Order(1)
+public class ExceptionFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
