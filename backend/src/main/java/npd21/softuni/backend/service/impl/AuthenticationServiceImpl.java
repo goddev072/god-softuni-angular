b13bd57f260eb0b530fc6a3e6032c510b75e47e4
goddev072 <god.dev0912@outlook.com>
2024-04-25 12:50:43 +0300

    java backend

---
+++ backend/src/main/java/npd21/softuni/backend/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,11 @@
+package npd21.softuni.backend.service.impl;
+
+import npd21.softuni.backend.service.AuthenticationService;
+
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+    @Override
+    public boolean authenticate(String email, String password) {
+        return false;
+    }
+}
